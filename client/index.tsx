275d972802774cc0494881416d78cca6d42d3d67
Peter Dravitzki <delcnz06@gmail.com>
2024-05-10 11:07:09 +1200

    working on

--- client/index.tsx
+++ client/index.tsx
@@ -1,7 +1,9 @@
 import { createRoot } from 'react-dom/client'
-
+//import Rotate from './components/Input.tsx'
 import App from './components/App.tsx'
 
 document.addEventListener('DOMContentLoaded', () => {
   createRoot(document.getElementById('app') as HTMLElement).render(<App />)
 })
+
+//document.getElementById("image")?.onclick=
