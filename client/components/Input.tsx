0ff0e196f1cf7f0821ee0d6a2226d36ed1039e4e
regie-malonzo <162513847+regie-malonzo@users.noreply.github.com>
2024-05-10 10:56:58 +1200

    enabled number limit

--- client/components/Input.tsx
+++ client/components/Input.tsx
@@ -1,29 +1,36 @@
 import { useState } from 'react'
 
 function Input() {
-  const [rotateAngle, setRotateAngle] = useState(0)
+  const [rotateAngle, setRotateAngle] = useState(1)
 
   const handleRotation = (newRotation: number) => {
     setRotateAngle(newRotation)
   }
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log('handleChange', event.target.value)
+    if (Number(event.target.value) > 360)
+      return alert('Angle is not between 0 and 360')
+
     setRotateAngle(Number.parseInt(event.target.value))
   }
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) =>
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
+    console.log('handleSubmit', rotateAngle)
+    setRotateAngle(1)
+  }
 
   return (
     <>
-      <p>asdasdasdasdasd</p>
+      <p>{rotateAngle}</p>
       <form onSubmit={handleSubmit}>
         <input
           onChange={handleChange}
           type="number"
           name="angle"
           id="angle"
-          value=""
+          value={rotateAngle}
         />
         <button type="submit">Submit</button>
       </form>
